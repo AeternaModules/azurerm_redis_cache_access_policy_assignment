8a622887241ffc4a494efcceb1cbbe38ad755686
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "redis_cache_access_policy_assignments" {
-  description = "All redis_cache_access_policy_assignment resources"
-  value       = azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments
-}
 output "redis_cache_access_policy_assignments_access_policy_name" {
-  description = "List of access_policy_name values across all redis_cache_access_policy_assignments"
-  value       = [for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : v.access_policy_name]
+  description = "Map of access_policy_name values across all redis_cache_access_policy_assignments, keyed the same as var.redis_cache_access_policy_assignments"
+  value       = { for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : k => v.access_policy_name }
 }
 output "redis_cache_access_policy_assignments_name" {
-  description = "List of name values across all redis_cache_access_policy_assignments"
-  value       = [for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : v.name]
+  description = "Map of name values across all redis_cache_access_policy_assignments, keyed the same as var.redis_cache_access_policy_assignments"
+  value       = { for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : k => v.name }
 }
 output "redis_cache_access_policy_assignments_object_id" {
-  description = "List of object_id values across all redis_cache_access_policy_assignments"
-  value       = [for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : v.object_id]
+  description = "Map of object_id values across all redis_cache_access_policy_assignments, keyed the same as var.redis_cache_access_policy_assignments"
+  value       = { for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : k => v.object_id }
 }
 output "redis_cache_access_policy_assignments_object_id_alias" {
-  description = "List of object_id_alias values across all redis_cache_access_policy_assignments"
-  value       = [for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : v.object_id_alias]
+  description = "Map of object_id_alias values across all redis_cache_access_policy_assignments, keyed the same as var.redis_cache_access_policy_assignments"
+  value       = { for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : k => v.object_id_alias }
 }
 output "redis_cache_access_policy_assignments_redis_cache_id" {
-  description = "List of redis_cache_id values across all redis_cache_access_policy_assignments"
-  value       = [for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : v.redis_cache_id]
+  description = "Map of redis_cache_id values across all redis_cache_access_policy_assignments, keyed the same as var.redis_cache_access_policy_assignments"
+  value       = { for k, v in azurerm_redis_cache_access_policy_assignment.redis_cache_access_policy_assignments : k => v.redis_cache_id }
 }
 
